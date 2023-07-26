56e65c9bd2c7f2962de8bf0d9d41b0edd39b9b23
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-26 12:31:59 +0100

    Create edit glove route

--- controllers/shop_controller.py
+++ controllers/shop_controller.py
@@ -29,7 +29,27 @@ def add_glove():
     price = request.form['price']
 
     glove = Glove(size=size, colour=colour, price=price)
-    
+
     db.session.add(glove)
+    db.session.commit()
+    return redirect("/gloves")
+
+@gloves_blueprint.route("/gloves/<id>/edit")
+def edit_glove(id):
+    glove = Glove.query.get(id)
+    return render_template("edit_glove.jinja", glove=glove)
+
+@gloves_blueprint.route("/gloves/<id>", methods=['POST'])
+def update_glove(id):
+    size = request.form['size']
+    colour = request.form['colour']
+    price = request.form['price']
+
+    glove = Glove.query.get(id)
+
+    glove.size = size
+    glove.colour = colour
+    glove.price = price
+
     db.session.commit()
     return redirect("/gloves")
